2509832fde9a0e1699f44fbd4d2ba6640edd467a
Kimi310 <94898434+Kimi310@users.noreply.github.com>
2025-05-02 09:47:36 +0200

    db functional and ready to use

--- Backend/Greenhouse.Domain/DatabaseDtos/Device.cs
+++ Backend/Greenhouse.Domain/DatabaseDtos/Device.cs
@@ -2,7 +2,7 @@ namespace Greenhouse.Domain.DatabaseDtos;
 
 public class Device
 {
-    public int Id { get; set; }
-    public int UserId { get; set; }
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
     public string DeviceName { get; set; }
 }
--- Backend/Greenhouse.Domain/DatabaseDtos/Preferences.cs
+++ Backend/Greenhouse.Domain/DatabaseDtos/Preferences.cs
@@ -2,8 +2,8 @@ namespace Greenhouse.Domain.DatabaseDtos;
 
 public class Preferences
 {
-    public int Id { get; set; }
-    public int DeviceId { get; set; }
+    public Guid Id { get; set; }
+    public Guid DeviceId { get; set; }
     public string SSID { get; set; }
     public string Passwordhash { get; set; }
     public int SensorInterval { get; set; }
