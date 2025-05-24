f7763e77400a067012937e75c4d57c92c2c88506
Kimi310 <94898434+Kimi310@users.noreply.github.com>
2025-05-24 14:47:56 +0200

    all tests for device controller finished

--- Backend/Greenhouse.Tests/DeviceControllerTests.cs
+++ Backend/Greenhouse.Tests/DeviceControllerTests.cs
@@ -19,10 +19,11 @@ public class DeviceControllerTests
     private HttpClient _httpClient;
     private IServiceProvider _scopedServiceProvider;
     private CookieContainer _cookieContainer;
+    private WebApplicationFactory<Program> _factory;
     [SetUp]
     public void Setup()
     {
-        var factory = new WebApplicationFactory<Program>()
+        _factory = new WebApplicationFactory<Program>()
             .WithWebHostBuilder(builder =>
             {
                 builder.ConfigureServices(services =>
@@ -31,12 +32,12 @@ public class DeviceControllerTests
                 });
             });
 
-        _httpClient = factory.CreateClient(new WebApplicationFactoryClientOptions
+        _httpClient = _factory.CreateClient(new WebApplicationFactoryClientOptions
         {
             BaseAddress = new Uri("http://localhost")
         });
 
-        _scopedServiceProvider = factory.Services.CreateScope().ServiceProvider;
+        _scopedServiceProvider = _factory.Services.CreateScope().ServiceProvider;
     }
 
 
@@ -45,6 +46,8 @@ public class DeviceControllerTests
     public void TearDown()
     {
         _httpClient?.Dispose();
+        (_scopedServiceProvider as IDisposable)?.Dispose();
+        _factory?.Dispose();
     }
 
     [Test]
@@ -146,7 +149,18 @@ public class DeviceControllerTests
     public async Task RemoveDeviceFromUser_ShouldReturnOk()
     {
         await ApiTestBase.TestRegisterAndSetAuthCookie(_httpClient);
+        var userInfo = await ApiTestBase.TestRegisterAndSetAuthCookie(_httpClient);
+        var dbContext = _scopedServiceProvider.GetService<AppDbContext>();
+
         var deviceId = Guid.NewGuid();
+        var device = new Device()
+        {
+            Id = deviceId,
+            DeviceName = "Test Device",
+            UserId = Guid.Parse(userInfo.UserId)
+        };
+        await dbContext.Devices.AddAsync(device);
+        await dbContext.SaveChangesAsync();
 
         var response = await _httpClient.SendAsync(new HttpRequestMessage
         {
@@ -155,30 +169,94 @@ public class DeviceControllerTests
             Content = JsonContent.Create(deviceId)
         });
 
+        dbContext.Entry(device).State = EntityState.Detached;
+
+        var deviceInDb = await dbContext.Devices
+            .Where(p => p.Id == deviceId)
+            .FirstOrDefaultAsync();
+
         Assert.That(response.IsSuccessStatusCode, Is.True);
+        Assert.That(deviceInDb, Is.Null);
     }
 
     [Test]
     public async Task MyDevices_ShouldReturnDeviceList()
     {
         await ApiTestBase.TestRegisterAndSetAuthCookie(_httpClient);
+        var userInfo = await ApiTestBase.TestRegisterAndSetAuthCookie(_httpClient);
+        var dbContext = _scopedServiceProvider.GetService<AppDbContext>();
+
+        var userId = Guid.Parse(userInfo.UserId);
+
+        var deviceId1 = Guid.NewGuid();
+        var device1 = new Device()
+        {
+            Id = deviceId1,
+            DeviceName = "Test Device 1",
+            UserId = userId
+        };
+        var deviceId2 = Guid.NewGuid();
+        var device2 = new Device()
+        {
+            Id = deviceId2,
+            DeviceName = "Test Device 2",
+            UserId = userId
+        };
+
+        await dbContext.Devices.AddRangeAsync(device1, device2);
+        await dbContext.SaveChangesAsync();
 
         var response = await _httpClient.GetAsync("Device/MyDevices");
 
         Assert.That(response.IsSuccessStatusCode, Is.True);
+
         var devices = await response.Content.ReadFromJsonAsync<List<DeviceResponseDto>>();
+
         Assert.That(devices, Is.Not.Null);
+        Assert.That(devices.Count, Is.EqualTo(2));
+
+        var deviceIds = devices.Select(d => d.Id).ToList();
+        Assert.That(deviceIds, Does.Contain(deviceId1));
+        Assert.That(deviceIds, Does.Contain(deviceId2));
+
+        var names = devices.Select(d => d.DeviceName).ToList();
+        Assert.That(names, Does.Contain("Test Device 1"));
+        Assert.That(names, Does.Contain("Test Device 2"));
     }
+
 
     [Test]
     public async Task UnassignedDevices_ShouldReturnDeviceList()
     {
         await ApiTestBase.TestRegisterAndSetAuthCookie(_httpClient);
+        var userInfo = await ApiTestBase.TestRegisterAndSetAuthCookie(_httpClient);
+        var dbContext = _scopedServiceProvider.GetService<AppDbContext>();
 
+        var userId = Guid.Parse(userInfo.UserId);
+
+        var deviceId1 = Guid.NewGuid();
+        var device1 = new UnassignedDevice()
+        {
+            Id = deviceId1,
+        };
+        var deviceId2 = Guid.NewGuid();
+        var device2 = new UnassignedDevice()
+        {
+            Id = deviceId2
+        };
+
+        await dbContext.UnassignedDevices.AddRangeAsync(device1, device2);
+        await dbContext.SaveChangesAsync();
         var response = await _httpClient.GetAsync("Device/UnassignedDevices");
 
         Assert.That(response.IsSuccessStatusCode, Is.True);
         var devices = await response.Content.ReadFromJsonAsync<List<UnassignedDevice>>();
-        Assert.That(devices, Is.Not.Null);
+        Assert.That(devices, Is.Not.Null, "Returned device list should not be null");
+        Assert.That(devices, Has.Count.EqualTo(2), "Returned device list should contain exactly 2 devices");
+
+        // Check that the returned devices contain the devices we added
+        Assert.That(devices.Any(d => d.Id == deviceId1), Is.True, "Device 1 should be in the returned list");
+        Assert.That(devices.Any(d => d.Id == deviceId2), Is.True, "Device 2 should be in the returned list");
+
     }
 }
