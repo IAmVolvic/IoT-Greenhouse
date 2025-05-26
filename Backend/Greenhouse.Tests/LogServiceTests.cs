b7ed06bcd0abac9999dc03edfde5c046ad19edb3
Kimi310 <94898434+Kimi310@users.noreply.github.com>
2025-05-26 19:20:31 +0200

    log service and passowrd manager tests

---
+++ Backend/Greenhouse.Tests/LogServiceTests.cs
@@ -0,0 +1,97 @@
+using System;
+using Greenhouse.Application.Mqtt.Dtos;
+using Greenhouse.Application.Repositories;
+using Greenhouse.Application.Services.Logs;
+using Greenhouse.Application.Websocket.DTOs;
+using Greenhouse.Application.Websocket.Interfaces;
+using Greenhouse.Domain.DatabaseDtos;
+using Greenhouse.Infrastructure.Services;
+using Moq;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Greenhouse.Tests;
+
+[TestFixture]
+public class LogServiceTests
+{
+    private Mock<ILogRepository> _logRepositoryMock;
+    private Mock<IConnectionManager> _connectionManagerMock;
+    private LogService _logService;
+
+    [SetUp]
+    public void Setup()
+    {
+        _logRepositoryMock = new Mock<ILogRepository>();
+        _connectionManagerMock = new Mock<IConnectionManager>();
+
+        _logService = new LogService(_logRepositoryMock.Object, _connectionManagerMock.Object);
+    }
+
+    [Test]
+    public void AddToDbAndBroadcast_ShouldAddLogToRepository()
+    {
+        // Arrange
+        var logDto = new DeviceLogDto
+        {
+            DeviceId = Guid.NewGuid(),
+            Unit = "°C",
+            Value = 25,
+            Type = "Temperature"
+        };
+
+        Log capturedLog = null;
+        _logRepositoryMock
+            .Setup(repo => repo.AddDeviceLog(It.IsAny<Log>()))
+            .Callback<Log>(log => capturedLog = log);
+
+        // Act
+        _logService.AddToDbAndBroadcast(logDto);
+
+        // Assert
+        _logRepositoryMock.Verify(r => r.AddDeviceLog(It.IsAny<Log>()), Times.Once);
+        Assert.That(capturedLog, Is.Not.Null, "Log was not captured");
+        Assert.That(capturedLog.DeviceId, Is.EqualTo(logDto.DeviceId));
+        Assert.That(capturedLog.Unit, Is.EqualTo(logDto.Unit));
+        Assert.That(capturedLog.Value, Is.EqualTo(logDto.Value));
+        Assert.That(capturedLog.Type, Is.EqualTo(logDto.Type));
+        Assert.That(capturedLog.Date, Is.Not.EqualTo(default(DateTime)), "Date was not set");
+    }
+
+    [Test]
+    public void AddToDbAndBroadcast_ShouldBroadcastLogToCorrectTopic()
+    {
+        // Arrange
+        var deviceId = Guid.NewGuid(); // Use a known GUID for reference
+        var logDto = new DeviceLogDto
+        {
+            DeviceId = deviceId,
+            Unit = "°C",
+            Value = 25,
+            Type = "Temperature"
+        };
+
+        ServerBroadcastsLogToDashboard capturedBroadcast = null;
+        string capturedTopic = null;
+
+        _connectionManagerMock
+            .Setup(cm => cm.BroadcastToTopic(It.IsAny<string>(), It.IsAny<ServerBroadcastsLogToDashboard>()))
+            .Callback<string, object>((topic, msg) =>
+            {
+                capturedTopic = topic;
+                capturedBroadcast = msg as ServerBroadcastsLogToDashboard;
+            })
+            .Returns(Task.CompletedTask);
+
+        // Act
+        _logService.AddToDbAndBroadcast(logDto);
+
+        // Assert
+        _connectionManagerMock.Verify(cm => cm.BroadcastToTopic(It.IsAny<string>(), It.IsAny<object>()), Times.Once);
+        Assert.That(capturedTopic, Is.EqualTo(deviceId.ToString()), "Broadcast topic mismatch");
+        Assert.That(capturedBroadcast, Is.Not.Null, "Broadcast message was null");
+        Assert.That(capturedBroadcast.log.DeviceId, Is.EqualTo(logDto.DeviceId));
+        Assert.That(capturedBroadcast.log.Unit, Is.EqualTo(logDto.Unit));
+    }
+
+}
